79f951451ee7232694c3b11257bc841ee482ae60
Matthew632 <matthew.hebb@hotmail.co.uk>
2019-03-24 18:28:09 +0000

    this commit changes the config file

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,15 @@
-const knex = require('knex');
-const dbConfig = require('../knexfile.js');
 
-const connection = knex(dbConfig);
+// const ENV = process.env.NODE_ENV || 'development';
+// const connection = ENV === 'production'
+//     ? { client: 'pg', connection: process.env.DATABASE_URL }
+//     : require('../knexfile');
 
-module.exports = connection;
+// module.exports = require('knex')(connection);
+
+
+const ENV = process.env.NODE_ENV || 'development';
+const config = ENV === 'production'
+  ? { client: 'pg', connection: process.env.DATABASE_URL }
+  : require('../knexfile');
+
+module.exports = require('knex')(config);
--- listen.js
+++ listen.js
@@ -1,8 +1,5 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) => {
-  if (err) throw err;
-  console.log(`Server listening on port ${PORT}...`);
-});
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
