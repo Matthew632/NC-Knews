c7ba86c3ee455ef86116958567d65b295fda3223
Matthew632 <matthew.hebb@hotmail.co.uk>
2019-04-07 12:09:29 +0100

    this commit adds a total count property to comment responses

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,5 +1,8 @@
 const {
-  fetchComments, postComment, amendVotes, delComment,
+  fetchComments,
+  postComment,
+  amendVotes,
+  delComment,
 } = require('../models/comments');
 const { fetchArticle } = require('../models/articles');
 
@@ -12,14 +15,24 @@ function getComments(req, res, next) {
   const columns = ['comment_id', 'votes', 'created_at', 'author', 'body'];
   if (limit !== undefined && !/^(0|[1-9]\d*)$/.test(limit)) next({ code: 400, msg: 'Limit should be a postive integer' });
   if (p !== undefined && !/^(0|[1-9]\d*)$/.test(p)) next({ code: 400, msg: 'Page should be a postive integer' });
-  if (limit === undefined) { limit = 10; }
-  if (p !== undefined) { page = limit * (p - 1); }
+  if (limit === undefined) {
+    limit = 10;
+  }
+  if (p !== undefined) {
+    page = limit * (p - 1);
+  }
   if (sort_by !== undefined && columns.every(col => sort_by !== col)) next({ code: 400, msg: 'Specified sort_by column does not exist' });
   if (order !== undefined && order !== 'asc' && order !== 'desc') next({ code: 400, msg: 'Order must be asc or desc' });
-  Promise.all([fetchComments(req.params, sort_by, order, limit, page), fetchArticle({ 'articles.article_id': req.params.article_id })])
-    .then(([fetchedComments, fetchedArticle]) => {
+  Promise.all([
+    fetchComments(req.params, sort_by, order, limit, page),
+    fetchArticle({ 'articles.article_id': req.params.article_id }),
+    fetchComments(req.params, sort_by, order),
+  ])
+    .then(([fetchedComments, fetchedArticle, commentCount]) => {
       if (fetchedArticle.length === 0) next({ code: 404, msg: 'Article not found' });
-      res.status(200).send({ comments: fetchedComments });
+      res
+        .status(200)
+        .send({ comments: fetchedComments, total_count: commentCount.length });
     })
     .catch(next);
 }
@@ -30,9 +43,10 @@ function insertComment(req, res, next) {
     body: req.body.body,
     article_id: req.params.article_id,
   };
-  postComment(newComment).then((postedComment) => {
-    res.status(201).send({ comment: postedComment[0] });
-  })
+  postComment(newComment)
+    .then((postedComment) => {
+      res.status(201).send({ comment: postedComment[0] });
+    })
     .catch(next);
 }
 
@@ -40,22 +54,29 @@ function patchComment(req, res, next) {
   const { inc_votes } = req.body;
   if (!Number.isInteger(inc_votes)) next({ code: 400, msg: 'Votes must be an integer' });
   else {
-    amendVotes(req.params, inc_votes).then((patchedComment) => {
-      res.status(200).send({ comment: patchedComment[0] });
-    })
+    amendVotes(req.params, inc_votes)
+      .then((patchedComment) => {
+        res.status(200).send({ comment: patchedComment[0] });
+      })
 
       .catch(next);
   }
 }
 
 function deleteComment(req, res, next) {
-  delComment(req.params).then((deleteCount) => {
-    if (deleteCount === 0) next({ code: 404, msg: 'Comment not found' });
-    else { res.sendStatus(204); }
-  })
+  delComment(req.params)
+    .then((deleteCount) => {
+      if (deleteCount === 0) next({ code: 404, msg: 'Comment not found' });
+      else {
+        res.sendStatus(204);
+      }
+    })
     .catch(next);
 }
 
 module.exports = {
-  getComments, insertComment, patchComment, deleteComment,
+  getComments,
+  insertComment,
+  patchComment,
+  deleteComment,
 };
